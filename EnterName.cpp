4e237f681e57f2b4ac014f7c022a30e8fb08bbe8
sam. khani <66492863+samakhani@users.noreply.github.com>
2023-02-04 03:15:19 +0330

    Enter Name

---
+++ EnterName.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[30];
+    cout << "Enter You name";
+    cout << "\n";
+    cin >> str;
+    cout << "Hello" << str << "You are learning cpp";
+    cout << endl;
+    return 0;
+}
